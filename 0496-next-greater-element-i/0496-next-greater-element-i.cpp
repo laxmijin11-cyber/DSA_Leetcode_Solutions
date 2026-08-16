db253c6ea3995689456606acfba60f54058c8d32
laxmijin11-cyber <laxmijin11@gmail.com>
2026-08-16 14:55:26 +0530

    Time: 4 ms (22.69%), Space: 12.8 MB (40.3%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> m;
+        stack<int>s;
+        //map mai store next greater
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(s.size()>0 && s.top()<=nums2[i]){
+                s.pop();
+            }
+            if(s.size()==0){
+                m[nums2[i]]=-1;
+            }
+            else{
+                m[nums2[i]]=s.top();
+            }
+            s.push(nums2[i]);
+        }
+
+        vector<int> ans(nums1.size(),0);
+        for(int i=0;i<nums1.size();i++){
+            ans[i]=m[nums1[i]];
+        }
+        return ans;
+    }
+};
