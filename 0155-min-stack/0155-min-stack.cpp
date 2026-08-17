c8ff8f0e123d832758396f2aba63fa31523de7f8
laxmijin11-cyber <laxmijin11@gmail.com>
2026-08-17 11:02:28 +0530

    Time: 73 ms (17.07%), Space: 151 MB (79.45%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,39 +1,64 @@
 class MinStack {
 public:
-    stack<long long int> s;
-    long long int minVal;
+    stack<pair<int,int>> s;
+    int minVal = INT_MAX;
     MinStack() {}
 
     void push(int value) {
         if (s.empty()) {
-            s.push(value);
-            minVal = value;
-        } else {
-            if (value < minVal) {
-                s.push((long long)2 * value - minVal);
-                minVal = value;
-            } else {
-                s.push(value);
-            }
+            minVal=value;
+            s.push({value, minVal});
+        } 
+        else {
+            minVal = min(value, s.top().second);
+            s.push({value, minVal});
         }
     }
 
-    void pop() {
-        if (s.top() < minVal) {
-            minVal = 2 * minVal - s.top();
-        }
-        s.pop();
+    void pop() { s.pop(); }
+
+    int top() { return s.top().first; }
+
+    int getMin() {
+        return s.top().second;
     }
 
-    int top() {
-        if (s.top() < minVal) {
-            return minVal;
-        } else {
-            return s.top();
-        }
-    }
+    // OPTIMISED APPROACH
 
-    int getMin() { return minVal; }
+    // stack<long long int> s;
+    // long long int minVal;
+    // MinStack() {}
+
+    // void push(int value) {
+    //     if (s.empty()) {
+    //         s.push(value);
+    //         minVal = value;
+    //     } else {
+    //         if (value < minVal) {
+    //             s.push((long long)2 * value - minVal);
+    //             minVal = value;
+    //         } else {
+    //             s.push(value);
+    //         }
+    //     }
+    // }
+
+    // void pop() {
+    //     if (s.top() < minVal) {
+    //         minVal = 2 * minVal - s.top();
+    //     }
+    //     s.pop();
+    // }
+
+    // int top() {
+    //     if (s.top() < minVal) {
+    //         return minVal;
+    //     } else {
+    //         return s.top();
+    //     }
+    // }
+
+    // int getMin() { return minVal; }
 };
 
 /**
