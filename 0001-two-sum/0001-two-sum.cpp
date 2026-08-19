c94e0e411e8ec7f61d239a44447c01841af2c148
laxmijin11-cyber <laxmijin11@gmail.com>
2026-08-19 12:01:21 +0530

    Time: 2 ms (72.17%), Space: 15 MB (25.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -15,5 +15,53 @@ public:
             m[first] = i;
         }
         return ans;
+        
     }
 };
+
+/*
+sort(nums.begin(), nums.end());
+        int first = 0;
+        int second = nums.size() - 1;
+        while (first < second) {
+            int sum = nums[first] + nums[second];
+            if (sum == target) {
+                return [first, second ];
+            } else if (sum > target) {
+                second--;
+            } else {
+                first++;
+            }
+        }
+        return [];
+
+
+        THIS IS WRONG AS:
+        But here, you are sorting the array first, which changes the order of elements. After sorting, the indices first and second refer to positions in the sorted array, not the original array.
+
+        vector<int> twoSum(vector<int>& nums, int target) {
+    vector<pair<int, int>> v;  // {value, original_index}
+    
+    for (int i = 0; i < nums.size(); i++) {
+        v.push_back({nums[i], i});
+    }
+    
+    sort(v.begin(), v.end());  // Sort by value
+    
+    int first = 0;
+    int second = v.size() - 1;
+    
+    while (first < second) {
+        int sum = v[first].first + v[second].first;
+        if (sum == target) {
+            return {v[first].second, v[second].second};
+        } else if (sum > target) {
+            second--;
+        } else {
+            first++;
+        }
+    }
+    
+    return {};
+}
+*/
