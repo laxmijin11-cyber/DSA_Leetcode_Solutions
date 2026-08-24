5ec1d0aef32e8a7e6f32d00fcdac466844e23eb5
laxmijin11-cyber <laxmijin11@gmail.com>
2026-08-24 13:31:55 +0530

    Create 2.cpp

---
+++ Recursion/2.cpp
@@ -0,0 +1,18 @@
+//FACTORIAL OF A NUMBER
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    cout << factorial(4);
+    return 0;
+}
