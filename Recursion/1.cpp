458d679be43191b7b999b9dc529956c80925f768
laxmijin11-cyber <laxmijin11@gmail.com>
2026-08-24 12:50:24 +0530

    Update 1.cpp

--- Recursion/1.cpp
+++ Recursion/1.cpp
@@ -1,1 +1,20 @@
+//PRINTING NUMBERS IN REVERSE FASHION 
+#include <bits/stdc++.h>
+using namespace std;
 
+void printNums(int n)
+{
+    if (n == 1)
+    {
+        cout << "1"<<" ";
+        return;
+    }
+    cout << n;
+    printNums(n - 1);
+}
+int main()
+{
+    printNums(10);
+    return 0;
+}
+//DONT USE printNums like it can cause error as print is keyword
