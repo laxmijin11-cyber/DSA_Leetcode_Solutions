47840c0827287f65c437226928b0651f5bf3fe5e
laxmijin11-cyber <laxmijin11@gmail.com>
2026-08-31 10:17:40 +0530

    Time: 0 ms (100%), Space: 23.7 MB (82.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,39 +1,17 @@
-// class Solution {
-// public:
-//     void moveZeroes(vector<int>& nums) {
-//         int left=0;
-//         int right=0;
-//         while(right<nums.size()){
-//             if(nums[right]!=0){
-//                 // nums[left]=nums[right];
-//                 // left++;
-//                 swap(nums[left],nums[right]);
-//                 left++;
-//                 right++;
-//             }
-//             else{
-//                 right++;
-//             }
-//         }
-//     }
-// };
-//right se check and left pe dalwa rahi hun.
-//nums = [0,1,0,3,12]
-
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0;  // Points to where the next non-zero should go
-        
-        for (int right = 0; right < nums.size(); right++) {
-            if (nums[right] != 0) {
-                // swap(nums[left], nums[right]);
-                int temp=nums[left];
-                nums[left]=nums[right];
-                nums[right]=temp;
-                left++;
+        int nonzeroindex=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[nonzeroindex]);
+                nonzeroindex++;
             }
         }
     }
 };
 
+/*
+nums = [0,1,0,3,12]
+0=j,
+*/
